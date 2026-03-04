ad7ad6934700efc3f359d1bf174b21678f94308b
Javier Gavidia <javiergavidia@gmail.com>
2026-03-04 19:52:22 +0100

    Prueba para subir a github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lista_de_tareas_react/", // Nombre del proyecto en github
+  base: "/lista_de_tareas_react_localstorage/", // Nombre del proyecto en github
 })
